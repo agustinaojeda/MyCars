d3115711668ed5fb8df9ebc3ea097f1914392e57
agustinaojeda <102766633+agustinaojeda@users.noreply.github.com>
2026-06-18 20:48:12 -0300

    footer

--- app/Views/index.php
+++ app/Views/index.php
@@ -183,6 +183,7 @@
     <?php else: ?>
         <p class="text-center p-4">Aún no hay vehículos disponibles</p>
     <?php endif; ?>
+    <?= view('templates/footer') ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 
